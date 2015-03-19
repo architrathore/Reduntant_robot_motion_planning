e2781bb84726cd8c70b4c8f5e12bc8f20f9ba078
architrathore <architrathore1@gmail.com>
2015-03-19 16:01:37 +0530

    Fixed intersections

--- robotplan.py
+++ robotplan.py
@@ -1,9 +1,10 @@
 import pygame, math, numpy
+from sklearn.neighbors import NearestNeighbors
 import fileinput
 
 coord = []
 running = 1
-numConfig = 1000
+numConfig = 100
 linecolor = 0, 200, 150
 robotcolor = 255,255,255
 bgcolor = 40, 40, 40
@@ -11,50 +12,66 @@ HEIGHT = 1000
 WIDTH = 1000
 screen = pygame.display.set_mode((HEIGHT,WIDTH))
 
+#--------------------------------------------------------------------------------------
+
+
+# def intersect (p1,p2,p3,p4):
+# 	par = (p1[0] - p2[0])*(p3[1] - p4[1]) - (p1[1]-p2[1])*(p3[0]-p4[0])
+# 	if(par!=0):
+# 		Px = ((p1[0]*p2[1] - p1[1]*p2[0])*(p3[0]-p4[0]) - (p1[0]-p2[0])*(p3[0]*p4[1]-p4[0]*p3[1]))/par
+# 		Py = ((p1[0]*p2[1] - p1[1]*p2[0])*(p3[1]-p4[1]) - (p1[1]-p2[1])*(p3[0]*p4[1]-p4[0]*p3[1]))/par
+# 		if(p1[0] <= Px and Px <= p2[0] and p3[0] <= Px and Px <= p4[0]) :
+# 			return True
+# 	else :
+# 		return False
+#--------------------------------------------------------------------------------------
 def ccw(p1,p2,p3):
 	return (p3[1]-p1[1]) * (p2[0]-p1[0]) > (p2[1]-p1[1]) * (p3[0]-p1[0])
 
 # Return true if line segments AB and CD intersect
 def intersect(p1,p2,p3,p4):
 	return ccw(p1,p3,p4) != ccw(p2,p3,p4) and ccw(p1,p2,p3) != ccw(p1,p2,p4)
+#--------------------------------------------------------------------------------------
 
-def theta_to_coordinate(theta_array,numDOF,link_len):
-	assert theta_array.shape[1] == numDOF
-	coord_matrix = numpy.zeros((theta_array.shape[0],2*(numDOF+1)))
-	coord_matrix[:,0] = 500
-	coord_matrix[:,1] = 500
-	for i in range(1,numDOF):
-		theta_array[:,i] += theta_array[:,i-1]
-	for i in range(1,numDOF+1):
-		coord_matrix[:,2*i] = coord_matrix[:,2*i-2] + numpy.cos(numpy.radians(theta_array[:,i-1]))*link_len[i-1]
-		coord_matrix[:,2*i+1] = coord_matrix[:,2*i-1] - numpy.sin(numpy.radians(theta_array[:,i-1]))*link_len[i-1]
-		# print x2.shape
-	return coord_matrix
+def intersect_obst(coord,obst_vertex):
+	for i in range(len(coord)-1):
+		x1y1 = coord[i]
+		x2y2 = coord[i+1]
+		for j in range(0,len(obst_vertex),2):
+			obst_x1y1 = obst_vertex[j]
+			obst_x2y2 = obst_vertex[j+1]
+			# print obst_x1y1,obst_x2y2
+			if(intersect(x1y1,x2y2,obst_x1y1,obst_x2y2)):
+				return True
 
-def generate_configs(numDOF,numConfig):
-	return numpy.random.rand(numConfig,numDOF)*360
+def theta_to_coord(angle_conf,numDOF,link_len):
+	theta = 0
+	xy_vector = []
+	xy_vector.append([0,0])
+	for i in range(numDOF):
+		theta = theta + angle_conf[i]
+		tempx = xy_vector[i][0] + link_len[i]*numpy.cos(numpy.radians(theta))
+		tempy = xy_vector[i][1] + link_len[i]*numpy.sin(numpy.radians(theta))
+		xy_vector.append([tempx,tempy])
+	return xy_vector
 
+def generate_valid_configs(numDOF,numConfig,obst_vertex,link_len):
+	valid_conf = []
+	i = 0
+	while(i < numConfig):
+		angle_conf = numpy.random.rand(numDOF)*360
+		coord = theta_to_coord(angle_conf,numDOF,link_len)
+		# print coord
+		if(intersect_obst(coord,obst_vertex)):
+			print "Ahh"
+			pass
+		else:
+			valid_conf.append(angle_conf)
+			if(intersect_obst(coord,obst_vertex)):
+				print "Nooooo"
+			i+=1
+	return valid_conf
 
-def valid_conf(robot_arm_coord, obst_vertex, numDOF, numObst):
-	intersect_idx = [False]*robot_arm_coord.shape[0]
-	flag = 0
-	for h in range(robot_arm_coord.shape[0]):
-		flag = 0
-		for i in range(0,2*numDOF,2):
-			robo_x1y1 = robot_arm_coord[h,i],robot_arm_coord[h,i+1]
-			robo_x2y2 = robot_arm_coord[h,i+2],robot_arm_coord[h,i+3]
-			for j in (0,2*numObst-2,2):
-				obst_x1y1 = obst_vertex[j]
-				obst_x2y2 = obst_vertex[j+1]
-				print j
-				if(intersect(robo_x1y1,robo_x2y2,obst_x1y1,obst_x2y2)):
-					flag = 1
-					break
-			if(flag==1):
-				break
-		if(flag==0):
-			intersect_idx[h]=True
-	return intersect_idx
 
 def robot_info(filename):
 	robot = open(filename,"r+").read().split("\n")
@@ -97,15 +114,10 @@ numDOF,link_len,obst_vertex = robot_info("robot.dat")
 draw_obst(obst_vertex)
 
 goal_angles = goal_info("goals.dat")
-theta_array = generate_configs(numDOF,numConfig)
-robot_arm_coord= numpy.array(theta_to_coordinate(theta_array,numDOF,link_len))
-intersect_idx = valid_conf(robot_arm_coord, numpy.array(obst_vertex), numDOF, len(obst_vertex)/2)
-print intersect_idx
-count = 0
-for i in range(len(intersect_idx)):
-	if(intersect_idx[i]):
-		count+=1
-print count
+theta_array = generate_valid_configs(numDOF,numConfig,obst_vertex,link_len)
+print theta_to_coord(goal_angles[0],numDOF,link_len)
+print
+print obst_vertex
 while running:
     event = pygame.event.poll()
     if event.type == pygame.QUIT:
@@ -115,10 +127,9 @@ while running:
     # pygame.draw.line(screen, linecolor, (0, y), (WIDTH - 1,y))
     # pygame.draw.aaline(screen, (0, 0, 255), (639, 0), (0, 479))
     # draw_obst(obst_vertex)
-    for i in range(robot_arm_coord.shape[0]):
-    	if(intersect_idx[i]== False):
-    		draw_robot(theta_array[i],link_len)
+    for i in range(len(theta_array)):
+    	draw_robot(theta_array[i],link_len)
+    draw_robot(goal_angles[0],link_len)
     draw_obst(obst_vertex)
-
     
     pygame.display.flip()
