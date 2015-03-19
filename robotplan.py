e9999e9f87b06843f94cc67d7f0ad54d6c9468fc
architrathore <architrathore1@gmail.com>
2015-03-19 16:16:15 +0530

    Cleaned prints and junk

--- robotplan.py
+++ robotplan.py
@@ -40,9 +40,10 @@ def intersect_obst(coord,obst_vertex):
 		for j in range(0,len(obst_vertex),2):
 			obst_x1y1 = obst_vertex[j]
 			obst_x2y2 = obst_vertex[j+1]
-			# print obst_x1y1,obst_x2y2
 			if(intersect(x1y1,x2y2,obst_x1y1,obst_x2y2)):
 				return True
+
+#--------------------------------------------------------------------------------------
 
 def theta_to_coord(angle_conf,numDOF,link_len):
 	theta = 0
@@ -55,23 +56,22 @@ def theta_to_coord(angle_conf,numDOF,link_len):
 		xy_vector.append([tempx,tempy])
 	return xy_vector
 
+#--------------------------------------------------------------------------------------
+
 def generate_valid_configs(numDOF,numConfig,obst_vertex,link_len):
 	valid_conf = []
 	i = 0
 	while(i < numConfig):
 		angle_conf = numpy.random.rand(numDOF)*360
 		coord = theta_to_coord(angle_conf,numDOF,link_len)
-		# print coord
 		if(intersect_obst(coord,obst_vertex)):
-			print "Ahh"
 			pass
 		else:
 			valid_conf.append(angle_conf)
-			if(intersect_obst(coord,obst_vertex)):
-				print "Nooooo"
 			i+=1
 	return valid_conf
 
+#--------------------------------------------------------------------------------------
 
 def robot_info(filename):
 	robot = open(filename,"r+").read().split("\n")
@@ -82,6 +82,7 @@ def robot_info(filename):
 		coord.append(map(int, robot[i].split(" ")))
 	return numDOF,link_len,coord
 
+#--------------------------------------------------------------------------------------
 def goal_info(filename):
 	config = open(filename,"r+").read().split("\n")
 	angles = []
@@ -89,15 +90,16 @@ def goal_info(filename):
 		angles.append(map(int,i.split(" ")))
 	return angles
 
+#--------------------------------------------------------------------------------------
 
 def draw_obst(obst_vertex):
-	# print obst_vertex
 	for i in range(0,len(obst_vertex),2):
 		x1,y1 = obst_vertex[i]
 		x2,y2 = obst_vertex[i+1]
 		pygame.draw.aaline(screen,linecolor,(WIDTH/2+x1,HEIGHT/2-y1),(WIDTH/2+x2,HEIGHT/2-y2))
 		# print "pygame.draw.aaline(screen,linecolor,(",x1,y1,"),(",x2,y2,"))"
 
+#--------------------------------------------------------------------------------------
 def draw_robot(angle_conf,link_len):
 	assert	len(angle_conf) == len(link_len)
 	angle_conf = list(angle_conf)
@@ -110,26 +112,22 @@ def draw_robot(angle_conf,link_len):
 		pygame.draw.aaline(screen,robotcolor,(x1,y1),(x2,y2))
 		x1,y1 = x2,y2
 
+#--------------------------------------------------------------------------------------
 numDOF,link_len,obst_vertex = robot_info("robot.dat")
 draw_obst(obst_vertex)
 
 goal_angles = goal_info("goals.dat")
 theta_array = generate_valid_configs(numDOF,numConfig,obst_vertex,link_len)
-print theta_to_coord(goal_angles[0],numDOF,link_len)
-print
-print obst_vertex
 while running:
     event = pygame.event.poll()
     if event.type == pygame.QUIT:
         running = 0
 
     screen.fill(bgcolor)
-    # pygame.draw.line(screen, linecolor, (0, y), (WIDTH - 1,y))
-    # pygame.draw.aaline(screen, (0, 0, 255), (639, 0), (0, 479))
-    # draw_obst(obst_vertex)
+    
     for i in range(len(theta_array)):
     	draw_robot(theta_array[i],link_len)
-    draw_robot(goal_angles[0],link_len)
     draw_obst(obst_vertex)
     
     pygame.display.flip()
+#--------------------------------------------------------------------------------------
